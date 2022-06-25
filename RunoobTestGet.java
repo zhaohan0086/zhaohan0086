ada5fcd0f7347dac8e33301c3f751f683596290f
zhaohan0086 <98364158+zhaohan0086@users.noreply.github.com>
2022-06-25 17:11:41 +0900

    Update and rename RunoobTest2.java to RunoobTestGet.java

--- RunoobTestGet.java
+++ RunoobTestGet.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
-public class RunoobTest2 {
+public class RunoobTestGet {
     public static void main(String[] args) {
         ArrayList<String> sites = new ArrayList<String>();
         sites.add("Google");
         sites.add("Runoob");
         sites.add("Taobao");
         sites.add("Weibo");
-        System.out.println(sites.get(1));  // 访问第二个元素
+        System.out.println(sites.get(1));  // 路脙脦脢碌脷露镁赂枚脭陋脣脴
     }
 }
