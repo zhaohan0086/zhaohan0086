ec186848a9c3853a2c1e8286421e9a3a0c0189fb
zhaohan0086 <98364158+zhaohan0086@users.noreply.github.com>
2022-06-25 17:21:31 +0900

    Add files via upload

---
+++ RunoobTestSize.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class RunoobTestSize {
+    public static void main(String[] args) {
+        ArrayList<String> sites = new ArrayList<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Weibo");
+        sites.remove(3); // 删除第四个元素
+        System.out.println(sites);
+    }
+}
