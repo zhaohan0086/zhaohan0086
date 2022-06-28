41935c24dd5e99db049a1cec8a31c64167b42c53
zhaohan0086 <98364158+zhaohan0086@users.noreply.github.com>
2022-06-28 20:48:36 +0900

    Add files via upload

---
+++ RunoobTestList.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class RunoobTestList {
+    public static void main(String[] args) {
+        LinkedList<String> sites = new LinkedList<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Weibo");
+        System.out.println(sites);
+    }
+}
