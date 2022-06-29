e5b76446bc4486e09242fe4f6caece5b99189ac4
zhaohan0086 <98364158+zhaohan0086@users.noreply.github.com>
2022-06-29 21:16:28 +0900

    Add files via upload

---
+++ RunoobTestListGetfirst.java
@@ -0,0 +1,14 @@
+// 引入 LinkedList 类
+import java.util.LinkedList;
+
+public class RunoobTestListGetfirst {
+    public static void main(String[] args) {
+        LinkedList<String> sites = new LinkedList<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Weibo");
+        // 使用 getFirst() 获取头部元素
+        System.out.println(sites.getFirst());
+    }
+}
