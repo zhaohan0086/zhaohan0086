d4c345453bcf9ae818402ac3016842f6173b7116
zhaohan0086 <98364158+zhaohan0086@users.noreply.github.com>
2022-06-29 21:17:59 +0900

    Add files via upload

---
+++ RunoobTestListGetlast.java
@@ -0,0 +1,14 @@
+// 引入 LinkedList 类
+import java.util.LinkedList;
+
+public class RunoobTestListGetlast {
+    public static void main(String[] args) {
+        LinkedList<String> sites = new LinkedList<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Weibo");
+        // 使用 getLast() 获取尾部元素
+        System.out.println(sites.getLast());
+    }
+}
