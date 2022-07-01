aa2814c7842ee3c6ddb5da23d698a34a6a5a49d4
zhaohan0086 <98364158+zhaohan0086@users.noreply.github.com>
2022-07-02 00:49:46 +0900

    Add files via upload

---
+++ RunoobHashSetClear.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class RunoobHashSetClear {
+    public static void main(String[] args) {
+    HashSet<String> sites = new HashSet<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Zhihu");
+        sites.add("Runoob");     // 重复的元素不会被添加
+        sites.clear();  
+        System.out.println(sites);
+    }
+}
