cd305e46ab40c046e43943aed6899ed89ab09180
zhaohan0086 <98364158+zhaohan0086@users.noreply.github.com>
2023-08-17 17:58:59 +0900

    Add files via upload

---
+++ SmallChangeSysAPP.java
@@ -0,0 +1,11 @@
+package com.zhaohan.smallchange;
+
+/**
+ * 这里我们直接调用Small----OOP 对象，显示主菜单即可
+ *
+ */
+public class SmallChangeSysAPP {
+    public static void main(String[] args) {
+        new SmallChangeSysOOP().mainMenu();
+    }
+}
