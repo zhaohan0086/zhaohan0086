ff4f49388fed523a761c00920e828ceeaa51d581
zhaohan0086 <98364158+zhaohan0086@users.noreply.github.com>
2023-08-22 21:21:27 +0900

    Add files via upload

---
+++ Homework03.java
@@ -0,0 +1,8 @@
+package com.zhaohan.homework;
+
+public class Homework03 {
+    public static void main(String[] args) {
+        Professor zhaoHan = new Professor("zhaoHan", 24, "It工程师", 18000, 3);
+        zhaoHan.introduce();
+    }
+}
